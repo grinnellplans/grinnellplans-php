b077b9e291a2475f3f787e6ecd1aa65834a09bec
theocharis.athanasakis <theocharis.athanasakis@259b1091-ff51-0410-bfa2-87932f29067e>
2008-09-28 02:26:43 +0000

    Bucketing.

---
+++ trunk/buckets/100.php
@@ -0,0 +1,3 @@
+<div style="position:absolute; top: 32px; right: 32px;">
+	<a href="http://www.collegehumor.com/video:1831461"><img height=200 width=200 src="buckets/100.gif" alt="Head of Skate" /></a><a href="bucket.php?b=50">x</a>
+</div>
---
+++ trunk/buckets/bucket.php
@@ -0,0 +1,15 @@
+<?
+	require_once('cookie_session.php');
+	$b = (int) $_GET['b'];
+	if (file_exists("buckets/$b.php")) {
+		$_SESSION['b'] = (int) $_GET['b'];
+	} else {
+		unset($_SESSION['b']);
+	}
+
+	if (isset($_SERVER['HTTP_REFERER'])) {
+		Header('Location: ' . $_SERVER['HTTP_REFERER']);
+	} else {
+		Header('Location: index.php');
+	}
+?>

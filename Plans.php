a478052a1cadaa0acc48c4a4021f4eed610ee551
theocharis.athanasakis <theocharis.athanasakis@259b1091-ff51-0410-bfa2-87932f29067e>
2008-09-28 03:58:27 +0000

    error reporting for beta

--- Plans.php
+++ Plans.php
@@ -1,11 +1,15 @@
 <?php
 	// Boilerplate code for _all_ Plans scripts
 
-	ini_set('error_reporting', E_ALL);
-	ini_set('register_globals',	FALSE);
-	ini_set('html_errors',	TRUE);
-	ini_set('include_path', '/inc');
+	// Set some testings only for when we're called through /beta/.
+	if (strstr($_SERVER['REQUEST_URI'], '/beta/') != FALSE) {
+		ini_set('error_reporting', E_ALL);
+		ini_set('register_globals',	FALSE);
+		ini_set('html_errors',	TRUE);
+	}
 
+	ini_set('include_path', '/inc');
+	
 	require_once("Configuration.php");	
 	require_once("cookie_session.php");
 	

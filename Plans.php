3e329775c01cd521df0b83962ef1931a40175287
theocharis.athanasakis <theocharis.athanasakis@259b1091-ff51-0410-bfa2-87932f29067e>
2008-09-28 03:55:31 +0000

    autoload, inc

--- Plans.php
+++ Plans.php
@@ -1,6 +1,18 @@
 <?php
 	// Boilerplate code for _all_ Plans scripts
-	
-	require_once("Configuration.php");
+
+	ini_set('error_reporting', E_ALL);
+	ini_set('register_globals',	FALSE);
+	ini_set('html_errors',	TRUE);
+	ini_set('include_path', '/inc');
+
+	require_once("Configuration.php");	
 	require_once("cookie_session.php");
+	
+	function __autoload($classname)
+	{
+		require_once("inc/$classname.php");
+	}
+	
+	header('Content-Type: text/html; charset=UTF-8');
 ?>

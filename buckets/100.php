a555a5c2b262c373de58ee9665a44f93b4f70d8f
theocharis.athanasakis <theocharis.athanasakis@259b1091-ff51-0410-bfa2-87932f29067e>
2008-09-28 20:45:36 +0000

    incude oopsie

--- buckets/100.php
+++ buckets/100.php
@@ -1,5 +1,5 @@
 <?php
-require_once ("../Plans.php");
+require_once("Plans.php");
 ?>
 <div style="position:absolute; top: 32px; right: 32px;">
 	<a href="http://www.collegehumor.com/video:1831461"><img height=200 width=200 src="buckets/100.gif" alt="Head of Skate" /></a><a href="bucket.php?b=50">x</a>

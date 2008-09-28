2087608f3835083e24b6f8905264f2879467d1d4
theocharis.athanasakis <theocharis.athanasakis@259b1091-ff51-0410-bfa2-87932f29067e>
2008-09-28 02:07:57 +0000

    Production worthy.

---
+++ tags/prod/changepassword.php
@@ -0,0 +1,81 @@
+<?
+
+
+require_once("cookie_session.php");
+require("functions-main.php");//load main functions
+$dbh = db_connect();//connect to database
+
+$idcookie = $_SESSION['userid']; 
+$auth = $_SESSION['is_logged_in'];
+
+if ( ! $auth) {
+	gdisp_begin($dbh);//begin guest display
+	echo("You are not allowed to edit as a guest.");//tell guest they can't use page
+	gdisp_end();
+} else {
+	mdisp_begin($dbh,$idcookie,$HTTP_HOST . $REQUEST_URI,$myprivl);//begin valid user display
+	$real_pass = get_item($mydbh,"guest_password", "accounts", "userid", $idcookie); 
+	$username = get_item($mydbh,"username", "accounts", "userid", $idcookie); 
+	if ($changed && ($checknumb != $idcookie)) {
+		echo "Error: Checknumbers do not match.";
+		exit(0);
+	}
+	if ($changed && ($mypassword != $mypassword2)) {
+		echo "Error: Passwords do not match.\n";
+		exit(0);
+	}
+	if ($changed=='pass') {
+		if (!(strstr($mypassword, "\"") or strstr($mypassword, "\'"))){
+			if (strlen($mypassword)>3){
+				$crpassword = crypt($mypassword, "ab"); //encrypt the password, 
+				set_item($dbh, "accounts", "password", $crpassword,
+						"userid", $idcookie);//set the password
+				echo "Password changed to <b>" . $mypassword . "</b>.";//confirm changed password
+			} else {echo "Could not change password. Your password must be 4 or more characters.";}
+		} else {echo "Illegal character in password. Please do not use \" or '.";}
+	} 
+	if ($changed=='guest_pass') {
+		$guest_password = $_POST['guest_password'];
+		set_item($mydbh,"accounts","guest_password", $guest_password, "userid", $idcookie); 
+		$real_pass = $guest_password;
+	}
+	?>
+		<center><h2>Change Login Password</h2>
+		<form method="POST" action="">
+<table>
+	<tr><td>New Password:</td><td><input type="password" name="mypassword"></td></tr>
+		<tr><td>Confirm: </td><td><input type="password" name="mypassword2"></td></tr></table> <br />
+		<input type="hidden" name="myprivl" value="<?php echo $myprivl; ?>">
+		<input type="hidden" name="changed" value="pass">
+		<input type="hidden" name="checknumb" value="<?php echo $idcookie; ?>">
+		<input type="submit" value="Change Password">
+		</form>
+		</center>
+		<center><h2>Set Guest Password</h2>
+		<p>
+		This is a password you can use to allow non-Plans users to read your plan.  They will not be able to edit your plan or use any other plans features. <br />
+This feature is intended to allow people to share their Plans with a small number of personal friends. 
+At any time, you may change this password to prevent people from accessing your plan using the old guest password.
+<br />
+<? if ($real_pass) { ?>
+You may give this link out to anyone who you would like to be able to read you plan and ask them to bookmark it: <br />
+<a href="http://www.grinnellplans.com/read.php?searchname=<?=$username?>&amp;guest-pass=<?=$real_pass?>">
+http://www.grinnellplans.com/read.php?searchname=<?=$username?>&amp;guest-pass=<?=$real_pass?>
+</a> <br />
+<? } else { ?>
+<b>Currently, your plan is completely private since you do not have a guest password set up.</b>
+<? } ?>
+		</p>
+		<form method="POST" action="">
+		<input type="text" name="guest_password" value="<?=$real_pass?>">
+		<input type="hidden" name="myprivl" value="<?php echo $myprivl; ?>">
+		<input type="hidden" name="changed" value="guest_pass">
+		<input type="hidden" name="checknumb" value="<?php echo $idcookie; ?>">
+		<input type="submit" value="Set Guest Password">
+		</form>
+		</center>
+		<?
+	mdisp_end($dbh,$idcookie,$HTTP_HOST . $REQUEST_URI,$myprivl);//end valid user display
+}
+db_disconnect($dbh);
+?>
---
+++ tags/prod/donations/donations.php
@@ -0,0 +1,193 @@
+<?php
+echo '<?xml version="1.0" encoding="ISO-8859-1"?>';
+echo "\n";
+include 'inc-db.php';
+include 'inc-error.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head><title>Donations Made to Support Plans</title></head>
+  <body>
+    <h2>Donations Made to Support Plans</h2>
+    <hr />
+
+<?php
+
+  foreach($_POST as $varname => $value)
+    $formVars[$varname] = $value;
+
+  // if (!($formVars["dbpass"])) {
+  //   $formVars["dbpass"] = "readonly";
+  //}
+  
+  if (!($connection = @ mysql_pconnect($hostName,$username,$password))) {
+   showError();
+   exit;
+  }  
+
+  if (!(mysql_select_db($databaseName, $connection))) {
+    showerror();
+    exit;
+  }
+
+if (!empty($_POST)) {
+ 
+  if (!($formVars["date"])) {
+     $formVars["date"] = "CURDATE()";
+  }
+  $insertQuery = "INSERT INTO donations (donor, amount, date, comment) values
+                    ('".$formVars["donor_name"]."','
+		    ".$formVars["amount_added"]."','
+		    ".$formVars["date"]."','
+		    ".$formVars["comments_added"]."')";
+
+  if (!(mysql_query($insertQuery, $connection))) {
+    showerror();
+  }
+
+}
+
+
+function p_print ($text) {
+  echo "<p>";
+  echo "$text";
+  echo "</p>";
+}
+
+
+function show_donations_table ($conn) {
+echo "    <table border=\"2\">
+      <tr>
+	<th>Donor</th>
+	<th>Amount ($)</th>
+	<th>Date</th>
+	<th>Comments</th>
+      </tr>";
+
+  $selectQuery = "SELECT donor, amount, DATE_FORMAT(date,'%m/%d/%Y'), comment FROM donations order by date desc, donor";
+  
+  if (!($infoz = mysql_query($selectQuery, $conn))) {
+    showerror();
+    exit;
+  }
+  if ($row = mysql_fetch_array($infoz)) {
+    do {
+      echo "<tr align=\"center\">\n\t<td>".$row["donor"]."</td>";
+      echo "\n\t<td>".$row["amount"]."</td>";
+      echo "\n\t<td>".$row["DATE_FORMAT(date,'%m/%d/%Y')"]."</td>";
+      echo "\n\t<td>".$row["comment"]."</td>";
+      echo "\n</tr>\n\n";
+    } while ($row = mysql_fetch_array($infoz));
+  }
+  echo "</table>";
+}
+
+function show_expenses_table($conn) {
+  echo "    <table border=\"2\">
+      <tr>
+	<th>Expense</th>
+	<th>Amount ($)</th>
+	<th>Date</th>
+      </tr>";
+
+  $selectQuery = "SELECT expense, amount, DATE_FORMAT(date,'%m/%d/%Y') FROM expenses order by date desc, expense";
+  
+  if (!($infoz = mysql_query($selectQuery, $conn))) {
+    showerror();
+    exit;
+  }
+  if ($row = mysql_fetch_array($infoz)) {
+    do {
+      echo "<tr align=\"center\">\n\t<td>".$row["expense"]."</td>";
+      echo "\n\t<td>".$row["amount"]."</td>";
+      echo "\n\t<td>".$row["DATE_FORMAT(date,'%m/%d/%Y')"]."</td>";
+      echo "\n</tr>\n\n";
+    } while ($row = mysql_fetch_array($infoz));
+  }
+
+
+  echo "</table>\n";
+
+}
+
+
+function get_table_total ($conn, $tbl) {
+  $sumquery = "SELECT SUM(amount) From $tbl";
+  if (!($result = mysql_query($sumquery, $conn))) {
+  showerror();
+  exit;
+  }
+  $val = mysql_fetch_array($result);
+  return $val[0];
+}
+
+function get_table_count($conn, $tbl) {
+  $countquery = "SELECT count(*) From $tbl";
+  if (!($result = mysql_query($countquery, $conn))) {
+  showerror();
+  exit;
+  }
+  $val = mysql_fetch_array($result);
+  return $val[0];
+}
+
+function show_statistics($conn) {
+  $donations_total = get_table_total($conn, 'donations');
+  $expenses_total = get_table_total($conn, 'expenses');
+  $difference = $donations_total - $expenses_total;
+  p_print("There has been <b>\$$donations_total</b> in donations and <b>\$$expenses_total</b> in expenses for a net total of <b>\$$difference</b>.");
+
+  $donations_count = get_table_count($conn, 'donations');
+  $expenses_count = get_table_count($conn, 'expenses');
+  p_print("There have been <b>$donations_count</b> donations and <b>$expenses_count</b> expense(s).");
+  
+}
+
+?>
+<table><tr><td><p align="center"><strong>Donations</strong></p>
+<?php
+show_donations_table($connection);
+?></td><td><p align="center"><strong>Expenses</strong></p>
+<?php
+show_expenses_table($connection);
+?></td></tr></table>
+<?php
+show_statistics($connection);
+  
+
+?>
+    <hr />
+    <p>
+      The dispensation of the fund is explained on <a href="http://www.cs.grin.edu/~wellons/plans/treasurer-duties.html">the treasurer's page</a>.
+    </p>
+    <hr />
+    <p>
+      Some individuals have been sending donations to:<br />
+      Jonathan Wellons<br />   
+      Box# 15 - 51<br />
+      Grinnell, IA 50112
+    </p>
+<p>
+Others are using the <a href="http://www.paypal.com">PayPal</a> account of 'wellons<!-- -->@cs.gr<!-- -->in.edu'. <b>PayPal charges a fee of 2.9% + $ 0.30 when you pay by credit card.</b>
+</p>
+    <p>
+      Some individuals who are donating anonymously are choosing a pseudonym so that they can verify that their donation appears in the table.
+</p>
+    <hr />
+    <p>
+      Questions and comments should be directed to: wellons@<!-- -->cs .grinnell.edu
+    </p>
+    
+    
+    <p>
+      <a href="http://validator.w3.org/check/referer"><img
+	  src="http://www.w3.org/Icons/valid-xhtml11"
+	  alt="Valid XHTML 1.1!" height="31" width="88" /></a>
+    </p>
+    
+    <p>
+      Created: Thursday, July 24, 2003
+    </p>
+    
+  </body>
+</html>

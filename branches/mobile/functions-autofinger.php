4e0f02a93fbee39313aaaacb02c5af8ec4042bea
mabonus <mabonus@259b1091-ff51-0410-bfa2-87932f29067e>
2008-10-11 15:15:05 +0000

    Creating branch for json api and mobile development

---
+++ branches/mobile/functions-autofinger.php
@@ -0,0 +1,91 @@
+<?php
+require_once('Plans.php');
+//////////
+/*
+setpriv - This function sets priviledge level
+*/
+function setpriv($myprivl, $cookpriv)
+{
+	$_SESSION['lvl'] = $myprivl;
+}
+//////////
+/*
+*Simply sets a plan that's on a person's autoread list to be marked as read
+*/
+function update_read($dbh, $owner, $updated)
+{
+	mysql_query("UPDATE autofinger SET updated = '0' WHERE owner = '$owner' and interest = '$updated'");
+}
+//////////
+/*
+*Marks when a person reads a plan
+*/
+function setReadTime($dbh, $idcookie, $interest)
+{
+	mysql_query("UPDATE autofinger SET readtime = NOW() WHERE owner = $idcookie AND interest = $interest");
+}
+function mark_as_read($dbh, $owner, $myprivl)
+{
+	$query = "UPDATE autofinger set updated = 0 where owner ='$owner' and priority = '$myprivl'";
+	mysql_query($query);
+}
+function add_param($url, $name, $value)
+{
+	if (ereg($name, $url)) {
+		return ereg_replace("$name=[^&]*", $name . '=' . $value, $url);
+	} else {
+		if (ereg("\?", $url)) {
+			return $url . '&' . $name . '=' . $value;
+		} else {
+			return $url . '?' . $name . '=' . $value;
+		}
+	}
+}
+function remove_param($url, $name)
+{
+	$url = ereg_replace("$name=[^&]*", '', $url);
+	$url = preg_replace(array("@&$@"), array(''), $url);
+	return $url;
+}
+function autoread_list($myurl, $idcookie, $myprivl)
+{
+	//echo '<!-- JLW ' . $myurl . "-->\n\n";
+	echo "</table>\n";
+	echo "<table>\n";
+	$mark_as_read = (isset($_GET['mark_as_read']) ? $_GET['mark_as_read'] : false);
+	if ($mark_as_read) {
+		mark_as_read($dbh, $idcookie, $myprivl);
+	}
+	for ($priority = 1; $priority < 4; $priority++) {
+//		$new_url = add_param('setpriv.php', 'myprivl', $priority);
+//		$new_url = remove_param($new_url, 'mark_as_read');
+		$new_url = "setpriv.php?myprivl=$priority";
+		echo '<tr><td></td><td><p class="imagelev2' . '">&nbsp;</p></td><td></td>' . "\n";
+		echo '<td><a href="' . $new_url . '" class="lev2' . '">level ' . $priority . '</a>' . "\n";
+		echo '</td>' . "\n";
+		if ($priority == $myprivl) {
+			$privarray = mysql_query("Select autofinger.interest,accounts.username
+			From autofinger, accounts where owner = '$idcookie' and priority =
+			'$myprivl' and updated = '1' and autofinger.interest=accounts.userid");
+			$autoreadlist = array();
+			while ($new_row = mysql_fetch_row($privarray)) {
+				$autoreadlist[] = $new_row;
+			}
+			echo '<td><a onClick =" ' . " return confirm('Are you sure you\'d like to mark all the Plans on level " . $priority . " as read?')" . '" href="http://' . add_param($new_url, 'mark_as_read', 1) . '">X</a></td></tr>' . "\n";
+			$o = 0;
+			while (isset($autoreadlist[$o]) && ($autoreadlist[$o][0])) {
+				$read_url = 'read.php';
+				$read_url = add_param($read_url, 'myprivl', $myprivl);
+				$read_url = add_param($read_url, 'searchname', $autoreadlist[$o][1]);
+				echo "<tr><td></td><td></td><td><p class=\"imagelev3\">&nbsp;</p></td>" . "\n";
+				echo "<td><a href=\"" . $read_url . "\" class=\"lev3\">" . "\n" . $autoreadlist[$o][1] . "</a></td></tr>\n";
+				$o++;
+			}
+			//echo '&nbsp;&nbsp;&nbsp;&nbsp;';
+			
+		} else {
+			echo "</tr>\n";
+		}
+	}
+}
+?>
---
+++ branches/mobile/kommand/index.php
@@ -0,0 +1,73 @@
+<?php
+require_once ("../Plans.php");
+require ("dbfunctions.php");
+$dbh = db_connect();
+/*
+print_r($_POST);
+echo "$username<br />\n";
+
+echo $_POST['username'] . " is the PoSTed username<br/>\n";
+*/
+if ($_POST['username'] == 'plans') {
+	$password = crypt($_POST['password'], "ab");
+	$read_pass = get_item($dbh, "password", "accounts", "username", "plans");
+	//echo "You gave $password, and we need $read_pass <br />\n";
+	if (($password == $read_pass) || $in) {
+		$_SESSION['kommand_auth'] = 1;
+		$_SESSION['kommand_logged_in'] = time();
+	} else {
+		$wrong_password = 1;
+	}
+}
+echo "<br />\n";
+if ($_SESSION['kommand_auth'] && (time() - $_SESSION['kommand_logged_in'] < 1800)) {
+?>
+<html>
+<body>
+<a href="adduser.php">Add a User</a><br>
+<a href="deleteuser.php">Delete User</a><br>
+<a href="changepassword.php">Change Password</a><br>
+<a href="changemotd.php">Change MOTD</a></br />
+<a href="truncate.php">Truncate a Plan</a><br />
+<a href="secrets.php">Manage Secrets</a><br />
+<a href="manage-donations.php">Manage Donations</a><br />
+<a href="/chat/usage.cgi">Chat Usage</a><br />
+<a href="polls.php">Manage Polls</a><br />
+<a href="new-accounts.cgi">New Account Usage </a><br />
+<a href="update-frequency.cgi">Update Frequency</a><br />
+<a href="swap-password.php">Switch a User's password with that of [test].</a><br />
+<pre>
+<?php
+	show_penetration();
+?>
+</pre>
+</body>
+</html>
+<?php
+} else if ($wrong_passwrod) {
+?>
+<html><body>Wrong password.<br><form action="index.php" method="POST">
+<input type="text" name="username"><Br>
+<input type="password" name="password"><br>
+<input type="submit" value="Kommand">
+</form>
+</body>
+</html>
+<?php
+} else {
+?>
+<html><body><form action="index.php" method="POST">
+<input type="text" name="username"><Br>
+<input type="password" name="password"><br>
+<input type="submit" value="Kommand">
+</form>
+</body>
+</html>
+<?php
+}
+db_disconnect($dbh);
+function show_penetration()
+{
+	system('/title/grinnellplans.com/class-year-penetration/run-counts.sh');
+}
+?>

adcaee26d3273c3ce2ae186cf307187fc0834fd3
ajlyon <ajlyon@259b1091-ff51-0410-bfa2-87932f29067e>
2008-10-23 08:09:26 +0000

    Making editing work, adding very basic styling for infoboxes.

--- branches/xhtml/edit.php
+++ branches/xhtml/edit.php
@@ -5,8 +5,10 @@ require ("functions-main.php"); //load main functions
 $dbh = db_connect(); ///connect to the database
 $idcookie = $_SESSION['userid'];
 $auth = $_SESSION['is_logged_in'];
+
 // Create the new page
-$page = new PlansPage('Main', 'edit', PLANSVNAME . ' - Edit Plan', 'edit.php'); //TODO
+$page = new PlansPage('Main', 'edit', PLANSVNAME . ' - Edit Plan', 'edit.php');
+
 if (!$auth) {
 	get_guest_interface();
 	populate_guest_page($page);
@@ -17,19 +19,25 @@ if (!$auth) {
 {
 	get_interface($idcookie);
 	populate_page($page, $dbh, $idcookie);
-	if (!$part) //if nothing submitted yet
+	if (!isset($_POST["plan"])) //if nothing submitted yet
 	{
-		$myedit = get_items($dbh, "plan,edit_cols,edit_rows,username", "accounts", "userid", $idcookie); //get plan as well as what the size of the plan should be and a username
-		$plantext = $myedit[0][0]; //put the contents of the plan into the plantext variable for easier use.
-		//Make sure that the size of the edit plan is above 0
+                //get plan as well as what the size of the plan should be and a username
+                $myedit = get_items($dbh, "plan,edit_cols,edit_rows,username", "accounts", "userid", $idcookie); 
+                $plantext = $myedit[0][0]; //put the contents of the plan into the plantext variable for easier use.
+
+                //Make sure that the size of the edit plan is above 0
 		if ($myedit[0][1] < 1) {
 			$myedit[0][1] = 70;
 		}
 		if ($myedit[0][2] < 1) {
 			$myedit[0][2] = 14;
 		}
-		//TODO needed?
-		//change <br>'s into \n's
+        
+                /**
+                 * This section of the code converts the HTML back into Plans
+                 * source. Another way to do this is to use the stored source.
+                 * */
+                //change <br>'s into \n's
 		$plantext = preg_replace("/<br>/", "", $plantext);
 		$plantext = preg_replace("/&amp;/s", "&", $plantext);
 		$plantext = preg_replace("/amp;/s", "", $plantext);
@@ -52,21 +60,28 @@ if (!$auth) {
 	} //if (!$part)
 	else
 	//if form info submitted, process
-	{
-		//TODO We got some diffing going on, but also a lot of cruft.
-		// Kill whatever isn't needed.
-		$diff_data = get_items($dbh, "edit_text", "accounts", "userid", $idcookie); //get plan as well as what the size of the plan should be
-		$old_plan = $diff_data[0][0]; //put the contents of the plan into the plan variable for easier use.
-		$diff = xdiff_string_diff($old_plan, $plan);
-		$diff = $diff;
-		mysql_query("insert into diffs(userid, text, date) values($idcookie, \"$diff\", now())");
-		//add_row($dbh, 'diffs', array($idcookie, $diff, 'now()'));
-		set_item($dbh, "accounts", "edit_text", $plan, "userid", $idcookie);
+        {
+                // Rename received text
+                $plan = $_POST["plan"];
+                
+                // Get the pre-edit text.
+                $old_plan = get_item($dbh, "edit_text", "accounts", "userid", $idcookie); 
+
+                // Take a diff versus the new version and store it.
+                $diff = xdiff_string_diff($old_plan, $plan);
+                mysql_query("insert into diffs(userid, text, date) values($idcookie, \"$diff\", now())");
+
+                // Store the edited plan source, convert it, and store the converted text.
+                set_item($dbh, "accounts", "edit_text", $plan, "userid", $idcookie);
 		$plan = cleanText($plan);
-		set_item($dbh, "accounts", "plan", $plan, "userid", $idcookie); //set plan in database
+                set_item($dbh, "accounts", "plan", $plan, "userid", $idcookie);
 		setUpdatedTime($idcookie); //set the time which keeps track of when the plan was last updated
 		set_item($dbh, "autofinger", "updated", 1, "interest", $idcookie); //make the plan show up as updated on other people's autoread list.
-		$plan = stripslashes($plan); //strip the slashes for display
+
+                // Rather than displaying the plan that was submitted, let's
+                // display the one that was stored. This should make truncation
+                // obvious.
+                $plan = get_item($dbh, "plan", "accounts", "userid", $idcookie);
 		$page->append(new InfoText($plan, "Plan Changed To:"));
 	} //if (!$part) else
 	

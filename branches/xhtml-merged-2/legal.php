2d3b37655cbab5901f74c6ff552cf294603e3ddc
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2009-01-12 00:18:45 +0000

    Updating the legalese

--- branches/xhtml-merged-2/legal.php
+++ branches/xhtml-merged-2/legal.php
@@ -18,38 +18,15 @@ function last_updated_plan() {
 	return $new_plans[1];
 }
 
+/**
+ * @deprecated use get_disclaimer()
+ */
 function disclaimer() {
-?>
-	<hr>
-	<p style="font-size: 80%">
-
-Use of the GrinnellPlans service means you have accepted the <a href="http://www.grinnellplans.com/tos"> GrinnellPlans Terms of Service agreement</a>. If you do not accept and abide by this agreement, you may not use GrinnellPlans. This agreement is subject to change without notice, so you should periodically review the most up-to-date version.
-You may contact us at 
-<script language="JavaScript" type="text/javascript">
-<!--
-var name = "grinnellplans";
-var domain = "gmail.com";
-document.write('<a href="mailto:' + name + '&#64;' + domain + '">');
- document.write(name + '&#64;' + domain);
-document.write('</a>');
-// -->
-</script>
-
-	<?php
+	echo get_disclaimer();
 }
 /* Same as above, only returns it as a string */
 function get_disclaimer()
 {
-	return "Use of the GrinnellPlans service means you have accepted the <a href=\"http://www.grinnellplans.com/tos\"> GrinnellPlans Terms of Service agreement</a>. If you do not accept and abide by this agreement, you may not use GrinnellPlans. This agreement is subject to change without notice, so you should periodically review the most up-to-date version.
-You may contact us at 
-<script language=\"JavaScript\" type=\"text/javascript\">
-<!--
-var name = \"grinnellplans\";
-var domain = \"gmail.com\";
-document.write('<a href=\"mailto:' + name + '&#64;' + domain + '\">');
- document.write(name + '&#64;' + domain);
-document.write('</a>');
-// -->
-</script>";
+	return '        <p>Use of the GrinnellPlans service means you have accepted the <a href="http://www.grinnellplans.com/tos"> GrinnellPlans Terms of Service agreement</a>. If you do not accept and abide by this agreement, you may not use GrinnellPlans. This agreement is subject to change without notice, so you should periodically review the most up-to-date version.You may contact us at <a href="mailto:grinnellplans@gmail.com">grinnellplans@gmail.com</a>.</p>';
 }
 ?>

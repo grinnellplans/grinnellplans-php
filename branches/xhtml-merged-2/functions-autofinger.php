ef746f4c3c3f4b7f5caefd2f1f3d9441ae8a217b
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2009-02-22 06:32:55 +0000

    Moving get_myprivl and removing unneeded file

--- branches/xhtml-merged-2/functions-autofinger.php
+++ branches/xhtml-merged-2/functions-autofinger.php
@@ -1,5 +1,13 @@
 <?php
 require_once('Plans.php');
+
+function get_myprivl() {
+	if (isset($_SESSION['glbs_lvl'])) {
+		return $_SESSION['glbs_lvl'];
+	} else {
+		return 1;
+	}
+}
 //////////
 /*
 setpriv - This function sets priviledge level

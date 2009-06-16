07176f66c6e7f15fe9c1bf23a46245892521c310
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-06-16 04:56:50 +0000

    minor syntax error

--- branches/xhtml-merged-2/views/templates/std/Hyperlink.tpl.php
+++ branches/xhtml-merged-2/views/templates/std/Hyperlink.tpl.php
@@ -1,2 +1,2 @@
 <a href="<?php echo $this->href; ?>" <?php echo $this->tag_attributes; ?>><?php echo 
-$this->description; ?></a><?php//TODO this kills the ending newline, but we shouldn't do this ?>
+$this->description; ?></a><?php //TODO this kills the ending newline, but we shouldn't do this ?>

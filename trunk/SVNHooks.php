091417cfec0830f766866064ec481b14e43d51bc
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-06-17 07:48:16 +0000

    svn hooks update

--- trunk/SVNHooks.php
+++ trunk/SVNHooks.php
@@ -16,7 +16,7 @@
 	}
 	
 	function background_exec($cmd) {
-		exec("$cmd 2>/dev/null >&- < &- >/dev/null &");
+		system("$cmd > /dev/null 2> /dev/null &");
 	}
 
 	$request = @file_get_contents('php://input');

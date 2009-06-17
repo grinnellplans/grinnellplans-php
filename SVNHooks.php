58100b728be242cabfba728fae9196a078ea99bc
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-06-17 03:48:46 +0000

    SVN Hooks for Google Code

---
+++ SVNHooks.php
@@ -0,0 +1,21 @@
+<?php
+	// http://code.google.com/p/support/wiki/PostCommitWebHooks
+
+	function hmac($key, $data, $hash = 'md5', $blocksize = 64) {
+		if (strlen($key)>$blocksize) {
+			$key = pack('H*', $hash($key));
+		}
+		$key = str_pad($key, $blocksize, chr(0));
+		$ipad = str_repeat(chr(0x36), $blocksize);
+		$opad = str_repeat(chr(0x5c), $blocksize);
+		return $hash(($key^$opad) . pack('H*', $hash(($key^$ipad) . $data)));
+	}
+
+	$request = @file_get_contents('php://input');
+	if (!isset($_SERVER['HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC']) ||
+		($_SERVER['HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC'] != hmac(GOOGLE_CODE_SECRET, $request))) {
+		die;
+	} else {
+		mail("thatha@thatha.org", "SVN updated", $request);
+	}
+?>

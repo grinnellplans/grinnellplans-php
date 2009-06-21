9c1890c7e35099d1c6b9adca68ede3030297b817
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-06-21 08:04:36 +0000

    tagging trunk as 2.7 Release

---
+++ tags/Grinnellplans-2.7-release/board_submit.php
@@ -0,0 +1,101 @@
+<?php
+require_once('Plans.php');
+require('functions-main.php');
+$dbh = db_connect(); //establish the database handler
+$idcookie = User::id();
+if (!User::logged_in()) {
+	gdisp_begin($dbh); 
+	echo ("You are not allowed to edit as a guest."); //tell person they can't log in
+	gdisp_end();
+} 
+else if (User::id() == 0) {
+	User::logout();
+}
+else
+//elseallowed to edit
+{
+	$showform = 1;
+	$error_message = '';
+	if ($submit) {
+		$showform = 0;
+		if ($newthread) {
+			$threadtitle = cleanText($threadtitle);
+			$threadtitle = preg_replace('/<[^>]*>/', '', $threadtitle);
+			if (!$threadtitle || preg_match("/JLW/", $threadtitle)) {
+				$showform = 1;
+				$error_message = "You are creating a new thread. Please enter a title for the thread.<br>";
+			} //if no threadtitle
+			
+		} //if newthread
+		else {
+			if (!(get_item($dbh, "threadid", "mainboard", "threadid", $threadid))) {
+				$showform = 1;
+				$error_message = "Invalid parent thread.<br>";
+			}
+		} //if not new thread
+		$messagetitle = cleanText($messagetitle);
+		$messagecontents = cleanText($messagecontents);
+		if (!$messagecontents) {
+			$showform = 1;
+			$error_message = "Please enter a message.<br>";
+		}
+		if (!$showform) {
+			if ($newthread) {
+				$my_result = mysql_query("Select threadid FROM mainboard WHERE lastupdated < DATE_SUB(NOW(), INTERVAL 7 DAY)");
+				while ($new_row = mysql_fetch_row($my_result)) {
+					//delete_item($dbh, "subboard", "threadid", $new_row[0]);
+					
+				}
+				//mysql_query("DELETE FROM mainboard WHERE lastupdated < DATE_SUB(NOW(), INTERVAL 7 DAY)");
+				mysql_query("INSERT INTO mainboard VALUES(\"\",\"" . addslashes($threadtitle) . "\",NOW(),NOW(),\"" . $idcookie . "\")");
+				$threadid = mysql_insert_id();
+			}
+			mysql_query("INSERT INTO subboard VALUES(\"\",\"" . $threadid . "\",NOW(),\"" . $idcookie . "\", \"" . addslashes($messagetitle) . "\", \"" . addslashes($messagecontents) . "\")");
+			mysql_query("UPDATE mainboard SET lastupdated = NOW() WHERE threadid = \"" . $threadid . "\"");
+			//			echo "Your message has been submitted.";
+			//process message here
+			
+		}
+	} //if submit
+	if ($error_message || $showform) {
+		mdisp_begin($dbh, $idcookie, $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], get_myprivl()); //begin user display
+		echo $error_message;
+	} else {
+		header('Location: http://www.grinnellplans.com/board_messages.php?threadid=' . $threadid);
+	}
+	if ($showform) {
+		echo "<form action=\"board_submit.php\" method=\"POST\">";
+		if ($newthread) {
+?>
+		Thread Title:<br><input type="text" name="threadtitle" value="<?php
+			echo $threadtitle
+?>"><br><br>
+		<input type="hidden" name="newthread" value="1">
+		<?php
+		} //if ($newthread)
+		$boardsize = get_items($dbh, "edit_cols,edit_rows", "accounts", "userid", $idcookie); //get the users chosen box size
+		if ($boardsize[0][1] < 1) {
+			$boardsize[0][0] = 70;
+		}
+		if ($boardsize[0][2] < 1) {
+			$boardsize[0][1] = 14;
+		}
+?>
+	Message Title:<br><input type="text" name="messagetitle" value="<?php
+		echo $messagetitle
+?>"><br><br>
+	Message Contents:<br><textarea rows="<?php
+		echo $boardsize[0][1] ?>" cols="<?php
+		echo $boardsize[0][0] ?>" name="messagecontents" 
+	wrap="virtual"><?php
+		echo $messagecontents ?></textarea><br>
+	<input type="hidden" name="threadid" value="<?php
+		echo $threadid ?>">
+	<input type="hidden" name="submit" value="1"><input type="submit" value="Submit Message"></form>
+	<?php
+	} //if showform
+	mdisp_end($dbh, $idcookie, $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], get_myprivl()); //gets user display
+	
+} //if valid user
+db_disconnect($dbh);
+?>
---
+++ tags/Grinnellplans-2.7-release/tests/json_api_test.php
@@ -0,0 +1,113 @@
+<?php
+//NOTE:  This has a url listed in TestOfJSONApi which points to a local dev URL
+//       This should point to your local environment.
+require_once('simpletest/autorun.php');
+require_once('simpletest/web_tester.php');
+
+
+class TestOfJSONApi extends WebTestCase {
+    private $url;
+    
+    function TestOfJSONApi(){ //setup URL
+      $this->url = 'http://creirwy/~michaelh/grinnellplans/json_api.php';
+    }
+    
+    function testConnection() { //Verify the server is running
+        $response = $this->get($this->url);
+        $this->assertTrue(($response !== false), "Check for server up");
+    }
+    
+    function testValidLogin() { //Verify that the login works
+      $response = $this->loginAs('youngian');
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->assertTrue(is_array($response->autofingerList), "Check for autofinger list");
+    }
+    
+    function testFailedLogin() {
+      $response = $this->post($this->url."?task=login", array("username"=>'youngian', "password" => 'bobo'));
+      $response = json_decode($response);
+      $this->assertFailure($response);
+    }
+    
+    function testAutofingerListLoginRequired() {
+      $response = $this->post($this->url."?task=autofingerlist");
+      $response = json_decode($response);
+      $this->assertFailure($response);
+    }
+    
+    function testAutofingerList() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=autofingerlist");
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+    }
+    
+    function testLoginRequiredRead() {
+      $response = $this->post($this->url."?task=read", array("username"=>'johnso58'));
+      $response = json_decode($response);
+      $this->assertFailure($response);
+    }
+    
+    function testReadingCompletePlan() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=read", array("username"=>'wrightjo'));
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->assertTrue(($response->partial == false), "checking for full response");
+      $this->assertTrue(is_object($response->plandata), "returning plan data");
+    }
+    
+    function testReadingPartialPlan() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=read", array("username"=>'johnso58', 'limitsize'=> true));
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->assertTrue(($response->plandata->partial == true), "checking for partial response");
+      $this->assertTrue(is_int($response->plandata->remaining), "checking for remaining size");
+      $this->assertTrue(is_object($response->plandata), "returning plan data");
+    }
+    
+    function testReadLink() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=read", array("username"=>'remarjen', 'readlinkreplacement' => 'fredlinktest{username}'));
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->assertTrue(is_string($response->plandata->plan), "returning content");
+      $this->assertTrue(stristr($response->plandata->plan, "fredlinktest"), "checking for replacement text");
+      $this->assertFalse(stristr($response->plandata->plan, "read.php"), "checking for read.php in plan text");
+    }
+    
+    function testReadingPartialPlanOnly() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=read", array("username"=>'johnso58', 'partial'=> true));
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->assertTrue(is_string($response->plandata->remainingplan), "checking returning remaining plan");
+    }
+    
+    function testReadingPartialPlanOnlyOnShortPlan() {
+      $this->loginAs('youngian');
+      $response = $this->post($this->url."?task=read", array("username"=>'cow', 'partial'=> true));
+      $response = json_decode($response);
+      $this->assertSuccess($response);
+      $this->asserttrue(empty($response->plandata->remainingplan), "checking returning remaining plan");
+    }
+    
+    
+    private function assertSuccess($response) {
+      $this->assertTrue(($response->success == 1), "Check for valid response");
+      $this->assertTrue(empty($response->message), "Check for empty message");
+    }
+    
+    private function assertFailure($response) {
+      $this->assertFalse($response->success, "Check for invalid response");
+      $this->assertFalse(empty($response->message), "Check for message");
+    }
+    
+    private function loginAs($username) {
+        return $this->post($this->url."?task=login", array("username"=>$username, "password" => $username ));
+    }
+    
+
+}

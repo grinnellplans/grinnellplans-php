6389874dfad264e46c06f84c3789fc4e7ae6c345
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2009-07-24 03:28:33 +0000

    Retagging 3.0

--- tags/Plans-3.0/views/templates/legacy/Form.tpl.php
+++
@@ -1,8 +0,0 @@
-<div<?php echo $this->tag_attributes; ?>><table><form method="<?php echo $this->method; ?>" action="<?php echo $this->action; ?>">
-<?php foreach ($this->contents as $template): ?>
-	<?php $template->display(); ?>
-<?php endforeach ?>
-	</table>
-		<input type="submit" value="<?php echo $this->submit_button->value; ?>">
-</form>
-</div>
--- tags/Plans-3.0/views/templates/tableless/NotesNavigation.tpl.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo $this->tag_attributes; ?>>
-<?php foreach (array('newest', 'even_newer', 'newer', 'current', 'older', 'even_older', 'oldest') as $linkname): ?>
-	<span class="<?php echo $linkname . ($this->navigable[$linkname]?' enabled':' disabled'); ?>"><?php $this->$linkname->display(); ?></span>
-<?php endforeach ?>
-</div>

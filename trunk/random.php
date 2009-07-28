031ee89bf44ac49ef2500031319b97fdacc4a685
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-07-28 01:30:45 +0000

    Fixes issue 74

--- trunk/random.php
+++ trunk/random.php
@@ -6,7 +6,7 @@ require('functions-main.php');
 $dbh = db_connect(); //get the database connection
 $idcookie = User::id();
 
-$result = mysql_query("select username from accounts order by rand() limit 1");
+$result = mysql_query("select username from accounts where length(plan) != 0 order by rand() limit 1");
 $result_row = mysql_fetch_array($result);
 $random_user = $result_row[0];
 header("Location: read.php?searchname=" . $random_user); //Send the user to that plan

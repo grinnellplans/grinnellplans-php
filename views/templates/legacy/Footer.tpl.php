74cfce1970c9be5dee1a98df18dd3b35b625f8d5
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-08-01 05:02:18 +0000

    Fixes issue 7. Fixes issue 103.

--- views/templates/legacy/Footer.tpl.php
+++ views/templates/legacy/Footer.tpl.php
@@ -2,6 +2,11 @@
 Do you read <?php $this->doyouread_link_template->display(); ?>, who just updated?<hr>	<hr>
 <?php endif ?>
 
+<div id="poweredby">
+<img src="images/logo-small.png">
+Powered by <a href="<?php echo ProjectInformation::projectUrl(); ?>">GrinnellPlans</a> <?php echo ProjectInformation::version(); ?>, an opensource project. File a <a href="<?php echo ProjectInformation::bugReportUrl(); ?>">bug report</a>.
+</div>
+
 <?php if ($this->legal_template): ?>
 <p style="font-size: 80%">
 		<?php $this->legal_template->display(); ?>

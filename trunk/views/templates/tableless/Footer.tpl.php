1b423d14a4884aa7eb54e31a1432f03af148aa84
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-08-01 05:02:18 +0000

    Fixes issue 7. Fixes issue 103.

--- trunk/views/templates/tableless/Footer.tpl.php
+++ trunk/views/templates/tableless/Footer.tpl.php
@@ -1,9 +1,18 @@
 <div id="footer">
 <?php if ($this->doyouread_link_template): ?>
-	<div id="justupdated"><div>
-		Do you read <?php $this->doyouread_link_template->display(); ?>, who just updated?
-	</div></div>
+	<div id="justupdated">
+		<div>Do you read <?php $this->doyouread_link_template->display(); ?>, who just updated?</div>
+	</div>
 <?php endif ?>
+
+
+<div id="poweredby">
+<div>
+<img src="images/logo-small.png" style="vertical-align: middle;" width="120px">
+Powered by <a href="<?php echo ProjectInformation::projectUrl(); ?>">GrinnellPlans</a> <?php echo ProjectInformation::version(); ?>, an opensource project. File a <a href="<?php echo ProjectInformation::bugReportUrl(); ?>">bug report</a>.
+</div>
+</div>
+
 <?php if ($this->legal_template): ?>
 	<div id="legal"><div>
 		<?php $this->legal_template->display(); ?>

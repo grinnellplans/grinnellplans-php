247c609c84074623708f7df1b7ff604a4325860b
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2009-08-09 07:04:41 +0000

    Configuring Doctrine stuff

--- trunk/Plans.php
+++ trunk/Plans.php
@@ -1,6 +1,5 @@
 <?php
-define('__ROOT__', dirname(__FILE__));
-require_once('Configuration.php');
+require_once('bootstrap.php');
 
 if (ENVIRONMENT == 'dev') {
 	// Error reporting for development
@@ -26,25 +25,8 @@ if (ENVIRONMENT == 'dev') {
 	$GLOBALS['ENVIRONMENT'] = 'production';
 }
 
-ini_set('include_path', '.:' . __ROOT__ . ':' . __ROOT__ . '/inc');
-putenv('TZ=' . TZ);
-
-// Doctrine setup
-require_once('lib/doctrine/lib/Doctrine.php');
-spl_autoload_register(array('Doctrine', 'autoload'));
-Doctrine_Manager::getInstance()->setAttribute('model_loading', 'conservative');
-Doctrine::loadModels(__ROOT__ . '/db'); // This call will not require the found .php files
-Doctrine_Manager::connection(DB_URI);
-
 // Simple functions
 require_once('functions.php');
-
-// Autoloader for classes
-function plans_autoload($classname) {
-	$filename = str_replace('_', '/', $classname);
-	require_once("$filename.php");
-}
-spl_autoload_register('plans_autoload');
 
 require_once('functions-main.php');
 

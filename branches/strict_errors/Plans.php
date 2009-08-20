728fc51685f72d08277f42d5285ef5936a002f26
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2009-08-20 04:25:55 +0000

    Cleaning up environment constants

--- branches/strict_errors/Plans.php
+++ branches/strict_errors/Plans.php
@@ -1,16 +1,12 @@
 <?php
 require_once('bootstrap.php');
 
-if (ENVIRONMENT == 'dev') {
-	//TODO For now, this is not a constant because I want to be able to change 
-	// it, but there's probably a better solution in the long run
-	$GLOBALS['ENVIRONMENT'] = 'development';
-} else {
-	$GLOBALS['ENVIRONMENT'] = 'production';
+if (!defined('ENVIRONMENT')) {
+	define('ENVIRONMENT', 'production');
 }
 
-if ($GLOBALS['ENVIRONMENT'] == 'development') {
-	ini_set('error_reporting', E_ALL);
+if (ENVIRONMENT == 'development') {
+	ini_set('error_reporting', E_ALL | E_STRICT);
 	ini_set('display_errors', TRUE);
 	ini_set('display_startup_errors', TRUE);
 	ini_set('html_errors', TRUE);
@@ -28,7 +24,7 @@ new SessionBroker();
 header('Content-Type: text/html; charset=UTF-8');
 
 // If we're on a testing environment, warn them
-if (User::logged_in() && $GLOBALS['ENVIRONMENT'] == 'testing' && !$_SESSION['accept_beta'] && basename($_SERVER['PHP_SELF']) != 'beta_warning.php') {
+if (User::logged_in() && ENVIRONMENT == 'testing' && !$_SESSION['accept_beta'] && basename($_SERVER['PHP_SELF']) != 'beta_warning.php') {
 	Redirect('beta_warning.php');
 	return;
 }

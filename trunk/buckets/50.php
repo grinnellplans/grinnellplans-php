619b1786142d91f5e5e3d62fd0edcb8b58070f66
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2009-12-10 13:48:56 +0000

    depracated code from ian's buckets'' experiment

--- trunk/buckets/50.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("Plans.php");
-?>
-<div style="position:absolute; top: 32px; right: 32px;">
-	<a href="http://www.collegehumor.com/video:1831461"><img height=110 width=110 src="buckets/100.gif" alt="Head of Skate" /></a><a href="bucket.php">x</a>
-</div>

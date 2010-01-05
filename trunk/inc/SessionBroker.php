beb2b710fe2b6810cf308566ebf7ad1849df8b6b
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2010-01-05 07:13:17 +0000

    Fixes issue 160

--- trunk/inc/SessionBroker.php
+++ trunk/inc/SessionBroker.php
@@ -8,6 +8,7 @@ require_once ('Configuration.php');
  */
 class SessionBroker {
     private static $instances = 0;
+    
     function SessionBroker() {
         if (SessionBroker::$instances == 0) {
             ob_start();
@@ -15,48 +16,63 @@ class SessionBroker {
             register_shutdown_function('session_write_close');
             session_set_cookie_params(0, '/', COOKIE_DOMAIN);
             session_start();
-            setcookie(session_name(), "", 0, '/', COOKIE_DOMAIN);
+            setcookie(session_name(), "", 0, '/', COOKIE_DOMAIN, false, true);
         }
         SessionBroker::$instances++;
     }
+    
     function open($arg_str_save_path, $arg_str_session_name) {
         return true;
     }
+    
     function close() {
         return true;
     }
+    
     function read($arg_str_session_id) {
         if (isset($_COOKIE[COOKIE_PAYLOAD])) {
             $cypher = $_COOKIE[COOKIE_PAYLOAD];
+            $given_signature = $_COOKIE[COOKIE_SIGNATURE];
             $td = mcrypt_module_open('tripledes', '', 'ecb', '');
             $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-            mcrypt_generic_init($td, SESSION_ENCRYPTION_KEY, $iv);
+            mcrypt_generic_init($td, COOKIE_ENCRYPTION_KEY, $iv);
             $plain_text = rtrim(mdecrypt_generic($td, base64_decode($cypher)), "\0");
             mcrypt_generic_deinit($td);
             mcrypt_module_close($td);
-            return $plain_text;
-        } else {
-            return "";
+            
+            $signature = base64_encode(md5(COOKIE_SIGNATURE_SALT . $plain_text, true));
+            
+            if ($signature == $given_signature) {
+                return $plain_text;
+            }
         }
+        return "";
     }
+    
     function write($arg_str_session_id, $arg_str_session_data) {
         if (!empty($arg_str_session_data)) {
             $td = mcrypt_module_open('tripledes', '', 'ecb', '');
             $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-            mcrypt_generic_init($td, SESSION_ENCRYPTION_KEY, $iv);
+            mcrypt_generic_init($td, COOKIE_ENCRYPTION_KEY, $iv);
             $cypher = base64_encode(mcrypt_generic($td, $arg_str_session_data));
             mcrypt_generic_deinit($td);
             mcrypt_module_close($td);
+            $signature = base64_encode(md5(COOKIE_SIGNATURE_SALT . $arg_str_session_data, true));
+            
             setcookie(session_name(), "", 0, "/", COOKIE_DOMAIN);
-            setcookie(COOKIE_PAYLOAD, $cypher, 0, "/", COOKIE_DOMAIN);
+            setcookie(COOKIE_PAYLOAD, $cypher, 0, "/", COOKIE_DOMAIN, false, true);
+            setcookie(COOKIE_SIGNATURE, $signature, 0, "/", COOKIE_DOMAIN, false, true);
             ob_end_flush();
         }
         return true;
     }
+    
     function destroy($arg_str_session_id) {
         setcookie(COOKIE_PAYLOAD, "", 0, "/", COOKIE_DOMAIN);
+        setcookie(COOKIE_SIGNATURE, "", 0, "/", COOKIE_DOMAIN);        
         return true;
     }
+    
     function gc($arg_int_next_lifetime) {
         return true;
     }

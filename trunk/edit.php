93b9564fb75e70e370263e03e56f7a9e3b5a577a
thatha7777 <thatha7777@259b1091-ff51-0410-bfa2-87932f29067e>
2010-09-29 00:16:39 +0000

    Issue 164. At the time edit.php is rendered, the state of a user's plan is now captured into an MD5 hash. This MD5 hash is embedded in the edit form. When the edit form gets submitted, the MD5 hash is checked against the MD5 hash of the plan in the database. If they don't much, it means that this plan was edited by someone else between rendering the edit form, and submitting the update plan. This will help prevent accidental overwrites on public plans. More importantly, it should prevent that nasty second POST. (By the time the second POST hits the server, the plan should be updated by the first plan, thereby invalidating the MD5 hash.

--- trunk/edit.php
+++ trunk/edit.php
@@ -23,12 +23,15 @@ if (!User::logged_in()) {
         }
         // Add the edit form
         $plantext = $user->Plan->edit_text;
-        $page->append(make_editbox($plantext, $user));
-        $log_msg = "Edit page loaded: id $idcookie, edit_text \"" . substr($plantext, 0, 50) . "..." . substr($plantext, -50) . '"';
+	$editbox = make_editbox($plantext, $user);       
+ 	$page->append($editbox);
+        $editbox->append(new HiddenInput('edit_text_md5', md5($plantext)));
+     	$page->append(new HiddenInput('sneak', '123'));
+	$log_msg = "Edit page loaded: id $idcookie, edit_text \"" . substr($plantext, 0, 50) . "..." . substr($plantext, -50) . '"';
         trigger_error($log_msg, E_USER_NOTICE);
-
     } else {
         // Rename received text
+        $pre_edit_md5 = $_POST["edit_text_md5"];
         $plan = $_POST["plan"];
 
         $log_msg = "Plan updated: id $idcookie, post \"" . substr($plan, 0, 50) . "..." . substr($plan, -50) . '"';
@@ -36,6 +39,12 @@ if (!User::logged_in()) {
 
         // Get the pre-edit text.
         $old_plan = $user->Plan->edit_text;
+	if (md5($old_plan) != $pre_edit_md5) {
+		$page->append(new AlertText("<br/>It seems that this plan was editted by someone else between you clicking the \"edit\" link and submitting your edits.", "Your plan was editted from another instance of the edit page, or there was a corruption while transferring data between the server and you."));
+		interface_disp_page($page);
+		db_disconnect($dbh);
+		exit;
+	}
         // Store the edited plan source, convert it, and store the converted text.
         try {
             $user->Plan->edit_text = $plan;
@@ -63,6 +72,7 @@ function make_editbox($plantext, $user) {
     $editbox = new EditBox($user->username, $plan, $user->edit_rows, $user->edit_cols);
     $editbox->action = 'edit.php';
     $editbox->method = 'post';
+    $editbox->append(new HiddenInput('edit_md5', '1'));
     return $editbox;
 }
 ?>

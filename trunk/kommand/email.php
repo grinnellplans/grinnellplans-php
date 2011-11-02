661e74e1843479a5f3ef8c67ed7a7f8b3ff9e630
alex@alexcohn.com <alex@alexcohn.com@259b1091-ff51-0410-bfa2-87932f29067e>
2011-11-02 00:11:19 +0000

    add support for AWS Simple Email Service to kommand's email feature

--- trunk/kommand/email.php
+++ trunk/kommand/email.php
@@ -1,19 +1,24 @@
 <?php
 require_once ("../Plans.php");
+require_once ("../functions-email.php");
 $username = $_POST['username'];
 require ("auth.php");
 ?>
 
 <?php
-$admin_email = "grinnellplans@gmail.com";
+$admin_email = MAILER_ADDRESS;
+if (!isset($_POST['whatoperation'])) die("Error: No operation specified.");
+$whatoperation = $_POST['whatoperation'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
 if ($whatoperation == "create") {
     $message = "An account for username " . $username . " with password " . $password . " has been set up for you.\n You can go to http://www.GrinnellPlans.com to log in.\nOnce you log in, you can change your password.";
-    mail($email, "Your new plan.", $message, "From:$admin_email\nReply-to:$admin_email");
-    echo "Message sent";
+    send_mail($email, "Your new plan.", $message, $admin_email, $admin_email)?echo "Message sent":echo "Message send failed";
 }
 if ($whatoperation == "changepassword") {
     $message = "Password for plan account " . $username . " changed to " . $password;
-    mail($email, "Plan password changed.", $message, "From:$admin_email\nReply-to:$admin_email");
+    send_mail($email, "Plan password changed.", $message, $admin_email, $admin_email);
     echo "Message sent";
 }
 ?>

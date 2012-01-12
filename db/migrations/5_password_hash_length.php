9e0d84fea3e9671945413f177eb9638cf2444e7b
alex <alex@alexcohn.com>
2012-01-12 03:28:14 +0000

    Turns out the crypt(3) man page lies about how long SHA-512 hashes are; it doesn't include the length of the salt in the 86-character limit it cites as the salt can be variable-length. Resolves Issue 229, at least until PHP switches the default hash to something even longer.

--- db/migrations/5_password_hash_length.php
+++ db/migrations/5_password_hash_length.php
@@ -1,7 +1,7 @@
 <?php
 class IncreaseHashLengthSHA512 extends Doctrine_Migration_Base {
     public function up() {
-        $this->changeColumn('accounts', 'password', '86', 'string');
+        $this->changeColumn('accounts', 'password', '128', 'string');
     }
     public function down() {
         $this->changeColumn('accounts', 'password', '34', 'string');

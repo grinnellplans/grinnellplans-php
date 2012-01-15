ade7c91d255f52aaf6ae42f9de3c31d48434cec3
alex@alexcohn.com <alex@alexcohn.com@259b1091-ff51-0410-bfa2-87932f29067e>
2012-01-15 20:47:19 +0000

    Amazon changed the SES API in a way that broke our email again. Fixing it.

--- trunk/functions-email.php
+++ trunk/functions-email.php
@@ -2,6 +2,8 @@
 require_once ('Plans.php');
 require_once ('lib/aws/sdk.class.php');
 
+CFCredentials::set(array('default'=>array('key'=>AWS_KEY,'secret'=>AWS_SECRET_KEY)));
+
 function send_mail($to, $subject, $text, $from, $reply_to) {
     if(USE_NATIVE_MAIL) {
         return mail($to, $subject, $text, "From:$from\nReply-to:$reply_to");
@@ -10,7 +12,7 @@ function send_mail($to, $subject, $text, $from, $reply_to) {
         $message = array('Subject'=>array('Data'=>$subject),
                          'Body'=>array('Text'=>array('Data'=>$text)));
         $opt = array('ReplyToAddresses'=>$reply_to);
-        $ses = new AmazonSES(AWS_KEY,AWS_SECRET_KEY);
+        $ses = new AmazonSES();
         $ret = $ses->send_email($from,array('ToAddresses'=>$to),$message,$opt);
         return $ret->isOK();
     }

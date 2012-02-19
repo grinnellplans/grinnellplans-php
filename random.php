b89c8802b756d4d34c10f26be64ea372ccd71a41
alex <alex@alexcohn.com>
2012-02-19 18:56:12 +0000

    Change random.php to only select plans that have been updated in the last year

--- random.php
+++ random.php
@@ -6,6 +6,6 @@ require_once ('Plans.php');
 new SessionBroker();
 require ('functions-main.php');
 
-$random_user = Doctrine_Query::create()->from('Accounts a')->leftJoin('a.Plan p')->where('LENGTH(p.edit_text) != 0')->orderby('RAND()')->limit(1)->fetchOne();
+$random_user = Doctrine_Query::create()->from('Accounts a')->leftJoin('a.Plan p')->where('LENGTH(p.edit_text) != 0')->andWhere('changed > DATE_SUB(NOW(), INTERVAL 1 YEAR)')->orderby('RAND()')->limit(1)->fetchOne();
 header("Location: read.php?searchname=" . $random_user->username);
 ?>

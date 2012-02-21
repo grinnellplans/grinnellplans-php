eeaaa975e9ea4f2232134a0b91db058024dd703c
alex@alexcohn.com <alex@alexcohn.com@259b1091-ff51-0410-bfa2-87932f29067e>
2012-02-21 05:05:17 +0000

    Avoid using "order by rand()" in random.php - it forces MySQL to create a temporary table containing the entire accounts and plans tables, which takes a write lock on those two tables, causing the site to become unresponsive until the query completes. Instead, calculate the size of the result set, generate a random number in PHP, and fetch just the row at that offset. Much faster, and doesn't create any locks.

--- trunk/random.php
+++ trunk/random.php
@@ -6,6 +6,7 @@ require_once ('Plans.php');
 new SessionBroker();
 require ('functions-main.php');
 
-$random_user = Doctrine_Query::create()->from('Accounts a')->leftJoin('a.Plan p')->where('LENGTH(p.edit_text) != 0')->andWhere('changed > DATE_SUB(NOW(), INTERVAL 1 YEAR)')->orderby('RAND()')->limit(1)->fetchOne();
+$random_query = Doctrine_Query::create()->select('a.username')->from('Accounts a')->leftJoin('a.Plan p')->where('LENGTH(p.edit_text) != 0')->andWhere('changed > DATE_SUB(NOW(), INTERVAL 1 YEAR)');
+$random_user = $random_query->offset(rand(0,$random_query->count()))->limit(1)->fetchOne();
 header("Location: read.php?searchname=" . $random_user->username);
 ?>

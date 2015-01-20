615ad0d9503cf8f701b20b960623a5ea6b105006
ian.greenleaf <ian.greenleaf@259b1091-ff51-0410-bfa2-87932f29067e>
2015-01-20 07:16:55 +0000

    Don't pick a blocked plan as random plan

--- branches/blocking/random.php
+++ branches/blocking/random.php
@@ -6,7 +6,19 @@ require_once ('Plans.php');
 new SessionBroker();
 require ('functions-main.php');
 
-$random_query = Doctrine_Query::create()->select('a.username')->from('Accounts a')->leftJoin('a.Plan p')->where('LENGTH(p.edit_text) != 0')->andWhere('changed > DATE_SUB(NOW(), INTERVAL 1 YEAR)');
+if (User::logged_in()) {
+    $ids_to_hide = Block::allUserIdsWithBlockingRelationships(User::id());
+    array_push($ids_to_hide, User::id());
+} else {
+    $ids_to_hide = [];
+}
+$random_query = Doctrine_Query::create()
+    ->select('a.username')
+    ->from('Accounts a')
+    ->leftJoin('a.Plan p')
+    ->where('LENGTH(p.edit_text) != 0')
+    ->andWhere('changed > DATE_SUB(NOW(), INTERVAL 1 YEAR)')
+    ->andWhereNotIn('a.userid', $ids_to_hide);
 $random_user = $random_query->offset(rand(0,$random_query->count()))->limit(1)->fetchOne();
 header("Location: read.php?searchname=" . $random_user->username);
 ?>

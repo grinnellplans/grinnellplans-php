45f8d81b0b10ce8d9ade2968e58996cbba3a8bde
Alex Cohn <alex@alexcohn.com>
2016-06-14 17:40:34 -0500

    Update project information footer URLs to point to GitHub, calculate revision hash correctly

--- inc/ProjectInformation.php
+++ inc/ProjectInformation.php
@@ -2,21 +2,15 @@
 require_once ("Plans.php");
 class ProjectInformation {
     public static function recentBugsUrl() {
-        return "http://code.google.com/p/grinnellplans/issues/list?sort=-id";
+        return "https://github.com/grinnellplans/grinnellplans-php/issues";
     }
     public static function projectUrl() {
-        return "http://code.google.com/p/grinnellplans/";
+        return "https://github.com/grinnellplans/grinnellplans-php/";
     }
     public static function revision() {
-        if (file_exists(__ROOT__ . '/.svn/entries')) {
-            $svn = file(__ROOT__ . '/.svn/entries');
-            if (is_numeric(trim($svn[3]))) {
-                $version = $svn[3];
-            } else {
-                // pre 1.4 svn used xml for this file
-                $version = explode('"', $svn[4]);
-                $version = $version[1];
-            }
+        if (file_exists(__ROOT__ . '/.git/refs/heads/master')) {
+            $git = file(__ROOT__ . '/.git/refs/heads/master');
+	    $version = substr($git[0],0,8);
             return trim($version);
         }
         return 0;
@@ -30,8 +24,8 @@ class ProjectInformation {
         $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $useragent = $_SERVER['HTTP_USER_AGENT'];
         $comment = "Username: [$username]\n" . "Revision: $revision\n" . "URL: $url\n" . "User-agent: $useragent\n\n" . "How can we reproduce the problem?\n1. \n2. \n3. \n\nWhat is the expected output? What do you see instead?\n ";
-        $parameters = "?comment=" . urlencode($comment);
-        return ProjectInformation::projectUrl() . "issues/entry" . $parameters;
+        $parameters = "?" . http_build_query(array('body' => $comment));
+        return ProjectInformation::projectUrl() . "issues/new" . $parameters;
     }
 }
 ?>
